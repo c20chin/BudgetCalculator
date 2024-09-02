624daf555488634ab1d9f63e7b96bbad287b8d42
Ju <c20chin@gmail.com>
2024-09-02 17:50:30 +0200

    initial

---
+++ BudgetCalculator/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BudgetCalculator.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
